1d0a73c6b63c15c4fb21758f74a01beb88158cea
Arthur Richon <richon.arthur@gmail.com>
2018-09-03 18:56:30 +0200

    Create React App init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Header from './Header';
+import Footer from './Footer';
+import './App.css';
+import './normalize.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <GridContainer/>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
