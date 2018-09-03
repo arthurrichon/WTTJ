003d5d0078067f4d56b3b167bc12c3d8bca4e37b
Arthur Richon <richon.arthur@gmail.com>
2018-09-03 18:57:42 +0200

    Header

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+			<header className="App-header">
+				<div className="App-Logo-Container">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to the jungle</h1>
+				</div>
+				<div className="App-Header-Controls">
+				</div>
+			</header>
+    );
+  }
+}
+
+export default Header;
